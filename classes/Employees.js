09e1b20661ddd4b963c77c210891fc117ff5a521
achasek <chaseknieper@gmail.com>
2023-11-08 13:31:06 -0700

    employees class made

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,7 +1,38 @@
 class Employees {
+    static #allEmployees = [];
+    #salary;
+    #isHired;
+    constructor(name, position, salary) {
+        this.name = name;
+        this.position = position;
+        this.#salary = salary;
+        this.#isHired = true;
+        // this.#allEmployees.push(Employees);
+    }
 
+    getSalary() {
+        return this.#salary;
+    }
 
+    setSalary(amount) {
+        this.#salary = amount;
+    }
 
+    getStatus() {
+        return this.#isHired;
+    }
+
+    setStatus(command) {
+        if(command === 'hire') {
+            this.#isHired = true;
+        } else if(command === 'fire') {
+            this.#isHired = false;
+        }
+    }
+
+    static getEmployees() {
+        return Employees.#allEmployees;
+    }
     
 }
 
